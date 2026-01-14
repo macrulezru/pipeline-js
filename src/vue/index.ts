73b53fab06726ddce033c739afe532dc245e551a
macrulezru <macrulezru@gmail.com>
2026-01-14 17:44:04 +0300

    fix: update imports in index files for Vue and React, and bump version to 1.1.2

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -1,3 +1,7 @@
 export * from "./usePipelineProgress";
 export * from "./usePipelineRun";
 export * from "./useRestClient";
+
+import "./usePipelineProgress";
+import "./usePipelineRun";
+import "./useRestClient";
